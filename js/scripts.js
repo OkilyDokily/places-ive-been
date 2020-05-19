4f6005627999a164e46e1c7a7476ad21770c9eec
me <me@me.me.com>
2020-05-19 13:11:28 -0700

    implement spec 1

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//Business logic
+function Place(city, country, landmarks, year, notes){
+  this.city = city,
+  this.country = country,
+  this.landmarks = landmarks,
+  this.year = year,
+  this.notes = notes
+}
+//UI Logic
+$(document).ready(function(){
+
+
+})
