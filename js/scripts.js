1cad1d91afb566728b9916ee180e7bcf9c843ce0
me <me@me.me.com>
2020-05-20 13:33:07 -0700

    make the project input based rather than hard coded

--- js/scripts.js
+++ js/scripts.js
@@ -6,79 +6,54 @@ function Place(city, country, year){
   this.landmarks = []
   this.notes = []
 }
-
-function PlaceHolder(){
-  this.places = [];
+Place.prototype.AddLandmark = function (string){
+  this.landmarks.push(string);
+}
+Place.prototype.AddNote = function (string){
+  this.notes.push(string);
 }
 
-function initializeProgram(){
-  PlaceHolder.prototype.AddPlace = function (place){
-    this.places.push(place);
-  }
-  Place.prototype.AddLandmark = function (string){
-    this.landmarks.push(string);
-  }
-  Place.prototype.AddNote = function (string){
-    this.notes.push(string);
-  }
-  var placeHolder = new PlaceHolder();
+var placeHolder = new PlaceHolder();
 
-  var paris = new Place("Paris","France",2000);
-  var newYork = new Place("New York City","USA",2002);
-  var sydney = new Place("Sydney","Australia", 1991);
-  var orlando = new Place("Orland","USA",1992);
   
-  paris.AddLandmark("Eiffel Tower");
-  paris.AddLandmark("Arc de Triomphe");
-  paris.AddNote("Was traveling in europe for 4 months at the time");
-  paris.AddNote("The camp grounds were bad, impossible to stake the tents");
-  
-  newYork.AddLandmark("Statue of Liberty");
-  newYork.AddLandmark("Ground Zero");
-  newYork.AddNote("The people were rude");
-  newYork.AddNote("The bars closed at 4 am");
-  
-  sydney.AddLandmark("Sydney Opera House");
-  sydney.AddLandmark("The Royal Botanic Gardens");
-  sydney.AddNote("I saw arsenic and old lace at the sydney opera house");
-  sydney.AddNote("As a kid I loved Australia");
-  
-  orlando.AddLandmark("Disney World");
-  orlando.AddLandmark("Universal Studios");
-  orlando.AddNote("I missed out on Nickelodean Studios because I was at space camp");
-  orlando.AddNote("Disney World is exactly like Disney Land")
-  
-  
-  placeHolder.AddPlace(paris);
-  placeHolder.AddPlace(newYork);
-  placeHolder.AddPlace(sydney);
-  placeHolder.AddPlace(orlando);
-
-  return placeHolder.places;
-}
-
 $(document).ready(function(){
 //ui logic
-  var places = initializeProgram();
-  places.forEach(function(place){
-    var id = place.city.split(' ').join('').toLowerCase();
-    $("#places").append("<li id='"+ id +"'>"+ place.city+"</li>")
-    $('#'+ id).append(function(){
-      $('#' + id).append("<ul class='visibility hide'>"
-      +"<li>" + place.country + "</li>"
-      + "<li>" + place.year + "</li>"
-      + "<li> Landmarks" 
-      +  "<ul class='landmarks'>"
-          
-      +  "</ul>"
-      + "</li>"
-      + "<li> Notes"
-      +  "<ul class='notes'>"
 
-      +  "</ul>"
-      + "</li>"
-      + "</ul>")
+  $("#addplace").submit(function(e){
+    e.preventDefault();
+    var city = $("#city").val();
+    var country = $("#country").val();
+    var year = $("#year").val();
+    
+    var landmarks = $("#landmarks").val().split(",");
+    var notes = $("#notes").val().split("/");
+
+    var place = new Place(city,country,year);
+    landmarks.forEach(function(landmark){
+      place.AddLandmark(landmark);
     });
+    notes.forEach(function(note){
+      place.AddNote(note);
+    })
+    
+    var id = place.city.split(' ').join('').toLowerCase();
+    $("#places").append("<li id='"+ id +"'>"+ place.city+"</li>");
+   
+    $('#' + id).append("<ul class='visibility hide'>"
+    +"<li>" + place.country + "</li>"
+    + "<li>" + place.year + "</li>"
+    + "<li> Landmarks" 
+    +  "<ul class='landmarks'>"
+        
+    +  "</ul>"
+    + "</li>"
+    + "<li> Notes"
+    +  "<ul class='notes'>"
+
+    +  "</ul>"
+    + "</li>"
+    + "</ul>");
+    
     place.landmarks.forEach(function(landmark){
       $("#" + id + " " + ".landmarks").append("<li>" + landmark + "</li>")
     })
@@ -89,7 +64,5 @@ $(document).ready(function(){
     $("#"+ id).click(function(){
       $("#"+ id +" " + ".visibility").toggleClass("hide");  
     });
-    
   });
- 
 })
